f971cf372435e85003b223bc1c94ef7d352f771e
norse900 <jmont.it@gmail.com>
2014-04-15 14:55:43 -0400

    Added character frequency counter

--- CryptoProtoCode/js/jsCode/letterFreq.js
+++ CryptoProtoCode/js/jsCode/letterFreq.js
@@ -2,5 +2,19 @@
  * Created by adminpc on 4/15/14.
  */
 function getLetterFreq(text){
-    return text;
+    var textArray ={};
+   for (var i = 0 ; i < text.length; i++){
+       if(textArray[text.charAt(i)]){
+           textArray[text.charAt(i)] += 1;
+          // console.log(textArray[text.charAt(i)]);
+       } else{
+           textArray[text.charAt(i)] = 1;
+          // console.log(textArray[text.charAt(i)]);
+       }
+   }
+    for(var g in textArray){
+        var value = textArray[g];
+    console.log("end item : "+ g+ ":" + textArray[g] );
+    }
+    return textArray;
 }
