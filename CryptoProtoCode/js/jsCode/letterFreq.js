0c888ba912789fe8b65bcb50aea052e61521dc62
norse900 <jmont.it@gmail.com>
2014-04-15 14:04:38 -0400

    Started setting up Specs

---
+++ CryptoProtoCode/js/jsCode/letterFreq.js
@@ -0,0 +1,6 @@
+/**
+ * Created by adminpc on 4/15/14.
+ */
+function getLetterFreq(text){
+    return text;
+}
---
+++ CryptoProtoCode/specs/cryptoSpecs.js
@@ -0,0 +1,18 @@
+/**
+ * Created by adminpc on 4/15/14.
+ */
+describe("Cryptogram Processing Specs", function(){
+    var plainLetterOne = "Wikipedia is a collaboratively edited, multilingual, free Internet encyclopedia that is supported by the non-profit Wikimedia Foundation. Volunteers worldwide collaboratively write Wikipedia's 30 million articles in 287 languages, including over 4.4 million ";
+    var cipherLetterOne = "RPGPFSHPL PA L TEKKLQEXLYPMSKJ SHPYSH, OWKYPKPUNWLK, VXSS PUYSXUSY SUTJTKEFSHPL YILY PA AWFFEXYSH QJ YIS UEU-FXEVPY RPGPOSHPL VEWUHLYPEU. MEKWUYSSXA REXKHRPHS TEKKLQEXLYPMSKJ RXPYS RPGPFSHPL'A 30 OPKKPEU LXYPTKSA PU 287 KLUNWLNSA, PUTKWHPUN EMSX 4.4 OPKK";
+    //Thanks to http://www.wordles.com/getmycrypto.aspx for the quick conversion.
+    describe("Analysis Specs", function(){
+        describe("Letter Frequency Specs", function(){
+            describe("Obtaining The Frequency Specs", function(){
+                it("should find the frequency of each letter", function(){
+                    var item = getLetterFreq(plainLetterOne);
+                    expect(item).not.toBeNull();
+                });
+            });
+        });
+    });
+});
