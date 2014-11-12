299ab3335d5c71df7d330f30985ce9ded6ed1a40
norse900 <WickedSHit1>
2014-11-12 10:13:17 -0500

    Started adding letter freqs to code. Reports the characters and their usage in the sentence.

--- CryptoProtoCode/specs/cryptoSpecs.js
+++ CryptoProtoCode/specs/cryptoSpecs.js
@@ -5,7 +5,39 @@ describe("Cryptogram Processing Specs", function () {
     var plainLetterOne = "Wikipedia is a collaboratively edited, multilingual, free Internet encyclopedia that is supported by the non-profit Wikimedia Foundation. Volunteers worldwide collaboratively write Wikipedia's 30 million articles in 287 languages, including over 4.4 million ";
     var cipherLetterOne = "RPGPFSHPL PA L TEKKLQEXLYPMSKJ SHPYSH, OWKYPKPUNWLK, VXSS PUYSXUSY SUTJTKEFSHPL YILY PA AWFFEXYSH QJ YIS UEU-FXEVPY RPGPOSHPL VEWUHLYPEU. MEKWUYSSXA REXKHRPHS TEKKLQEXLYPMSKJ RXPYS RPGPFSHPL'A 30 OPKKPEU LXYPTKSA PU 287 KLUNWLNSA, PUTKWHPUN EMSX 4.4 OPKK";
     //Thanks to http://www.wordles.com/getmycrypto.aspx for the quick conversion.
-    describe("Analysis Specs", function () {
+
+    //http://www.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
+    /*
+     Letter	Count	 	Letter	Frequency
+     E	21912	 	E	12.02
+     T	16587	 	T	9.10
+     A	14810	 	A	8.12
+     O	14003	 	O	7.68
+     I	13318	 	I	7.31
+     N	12666	 	N	6.95
+     S	11450	 	S	6.28
+     R	10977	 	R	6.02
+     H	10795	 	H	5.92
+     D	7874	 	D	4.32
+     L	7253	 	L	3.98
+     U	5246	 	U	2.88
+     C	4943	 	C	2.71
+     M	4761	 	M	2.61
+     F	4200	 	F	2.30
+     Y	3853	 	Y	2.11
+     W	3819	 	W	2.09
+     G	3693	 	G	2.03
+     P	3316	 	P	1.82
+     B	2715	 	B	1.49
+     V	2019	 	V	1.11
+     K	1257	 	K	0.69
+     X	315	 	X	0.17
+     Q	205	 	Q	0.11
+     J	188	 	J	0.10
+     Z	128	 	Z	0.07
+    * \*/
+    // ETAOINSRHDLUCMFYWGPBVKXOJZ
+     describe("Analysis Specs", function () {
         describe("Letter Frequency Specs", function () {
             describe("Obtaining The Frequency Specs", function () {
                 it("should find the frequency of each letter", function () {
@@ -13,20 +45,25 @@ describe("Cryptogram Processing Specs", function () {
                     expect(item).not.toBeNull();
                 });
 
-                    it("should have no zero percent letters", function () {
-                        var item = getLetterFreq(plainLetterOne);
-                        var zeroPercentLetterFound = false;
-                        //I don't like this and it needs some serious refactoring. I've been up for 33 hours now and am having logical thinking issues.
-                        for (var i in item) {
-                            console.log(" i : " + i);
-                            if(i.indexOf("%1") >= 0){
-                                zeroPercentLetterFound = true;
-                                return;
-                            }
+                it("should have no zero percent letters", function () {
+                    var item = getLetterFreq(plainLetterOne);
+                    var zeroPercentLetterFound = 0;
+                    //I don't like this and it needs some serious refactoring. I've been up for 33 hours now and am having logical thinking issues.
+                    for(var i in item) {
+
+                        if (i.charAt(0).indexOf("0") < 0 && i.charAt(1).indexOf("0") > 0) {
+                            zeroPercentLetterFound = 1;
+                            return;
+                        } else {
+                            //console.log(" i : " + i.charAt(0) + " - " + i.charAt(1));
+                            console.log( i + " : "  + item[i]);
+                            //console.log(" i.(0) : " + i.charAt(0) + " i.(1) : " + i.charAt(1));
                         }
-                        expect(zeroPercentLetterFound == false);
-                    });
-                    it("should list the letters and their frequency", function () {  });
+                    }
+                    expect(zeroPercentLetterFound).not.toBeGreaterThan(0);
+                });
+                it("should list the letters and their frequency", function () {
+                });
             });
         });
     });
